f9b9778c41d17c313470d0120f54831a455d535e
Ray Cai Flanagan <hjkareus@gmail.com>
2024-11-12 11:06:37 -0500

    add database init

--- src/crud/index.ts
+++ src/crud/index.ts
@@ -1,1 +1,9 @@
 const pgp = require('pg-promise')();
+
+export const db = pgp({
+    host: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+});
