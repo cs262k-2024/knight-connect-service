1995ee60e943eb386e0086fe88674e2a2f80583e
Alex Ye <albertexye@gmail.com>
2024-11-14 21:55:34 -0500

    support TS type hint for pg promise

--- src/crud/index.ts
+++ src/crud/index.ts
@@ -1,9 +1,12 @@
-const pgp = require('pg-promise')();
+import pgPromise from "pg-promise";
+
+const pgp = pgPromise({});
 
 export const db = pgp({
-    host: process.env.DB_SERVER,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.DB_SERVER!,
+    port: Number(process.env.DB_PORT!),
+    database: process.env.DB_DATABASE!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    ssl: true,
 });
